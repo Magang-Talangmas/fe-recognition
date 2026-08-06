434f25d5a1a9eb592cfb364e0522f114d6940e92
tiara1210 <111202315347@mhs.dinus.ac.id>
2026-08-06 10:33:20 +0700

    style(table): improve table styling

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -57,7 +57,7 @@ function TableRow({ className, ...props }: React.ComponentProps<"tr">) {
     <tr
       data-slot="table-row"
       className={cn(
-        "transition-colors hover:bg-muted/50 has-aria-expanded:bg-muted/50 data-[state=selected]:bg-muted",
+        "border-b-2 border-border transition-colors hover:bg-muted/50 has-aria-expanded:bg-muted/50 data-[state=selected]:bg-muted",
         className
       )}
       {...props}
@@ -70,7 +70,7 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
     <th
       data-slot="table-head"
       className={cn(
-        "h-10 px-2 text-left align-middle font-medium whitespace-nowrap text-foreground bg-muted/50 [&:has([role=checkbox])]:pr-0",
+        "h-10 px-2 text-left align-middle font-medium whitespace-nowrap text-foreground bg-muted/50 border-b-2 border-border [&:has([role=checkbox])]:pr-0",
         className
       )}
       {...props}
